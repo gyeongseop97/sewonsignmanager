7ee1228bf3c39a7fe2764da3205b850d113bbf47
gyeongseop97 <gyeongseop97@gmail.com>
2026-06-23 15:50:13 +0900

    x

--- app-repair.js
+++ app-repair.js
@@ -1,13 +1,13 @@
 (async function(){
   try{
     var st=document.createElement('script');
-    st.src='stable-login.js?v=1';
+    st.src='stable-login.js?v=2';
     document.head.appendChild(st);
     var u=document.createElement('script');
     u.src='octomo-qr-ui.js?v=restore-3';
     document.head.appendChild(u);
     var h='https://raw.githubusercontent.com/';
-    var p='gyeongseop97/sewonsignmanager/494d503165374d896da2ed155476aadd8fa6b8b1/app.js';
+    var p='gyeongseop97/sewonsignmanager/494d503165374d896da2ed155476aadd8fa6b8b11aa6/app.js';
     var res=await fetch(h+p,{cache:'no-store'});
     if(!res.ok) throw new Error('load failed');
     var code=await res.text();
