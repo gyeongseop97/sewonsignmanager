a6f4c26ea2015e3f4b937439888ee6fa23ade780
gyeongseop97 <gyeongseop97@gmail.com>
2026-06-23 13:21:23 +0900

    Add repaired app loader

---
+++ app-repair.js
@@ -0,0 +1,12 @@
+(async function(){
+  try{
+    var h='https://raw.githubusercontent.com/';
+    var p='gyeongseop97/sewonsignmanager/494d503165374d896da2ed155476aadd8fa6b8b1/app.js';
+    var res=await fetch(h+p,{cache:'no-store'});
+    if(!res.ok) throw new Error('load failed');
+    var code=await res.text();
+    Function(code)();
+  }catch(e){
+    alert('초기화 실패: '+(e.message||e));
+  }
+})();
